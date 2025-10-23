c285a2fefaa45729ee4642cbcb37ab61f92cca7d
JulianAballay <juliancabj1210@gmail.com>
2025-10-23 01:01:14 -0300

    centro el h1 del titulo

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <section>
       <Container>
         <TituloVeterinaria />
-        <FormularioVeterinaria /  >
+        <FormularioVeterinaria />
       </Container>
     </section>
   );
--- src/components/TituloVeterinaria.jsx
+++ src/components/TituloVeterinaria.jsx
@@ -1,7 +1,7 @@
 
 const TituloVeterinaria = () => {
     return (
-        <h1 className="display-2 mt-3">Administrar pacientes de veterinaria</h1>
+        <h1 className="display-2 my-4 text-center">Administrar pacientes de veterinaria</h1>
     );
 };
 
